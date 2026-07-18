7eec23b80eee954b82b952ea4ded4b46b31d33e5
Dusan <arhistrategstudio@gmail.com>
2026-07-18 12:07:22 +0200

    Faza 7: izmenjena imena i kolicine proizvoda, uklonjen prikaz inventara

--- main.py
+++ main.py
@@ -3,10 +3,7 @@ from product_manager import ProductManager
 
 manager = ProductManager()
 
-manager.add_product(Product("Laptop", 89990.00, 5))
-manager.add_product(Product("Mis", 2490.00, 20))
-manager.add_product(Product("Tastatura", 4990.00, 12))
-manager.add_product(Product("Monitor", 24990.00, 7))
-
-manager.show_all_products()
-manager.total_inventory_value()
+manager.add_product(Product("Gaming laptop", 89990.00, 3))
+manager.add_product(Product("Bezicni mis", 2490.00, 25))
+manager.add_product(Product("Mehanicka tastatura", 4990.00, 10))
+manager.add_product(Product("Monitor 27 inch", 24990.00, 4))
